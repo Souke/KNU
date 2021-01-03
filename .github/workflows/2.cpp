abe96f4329229c756f3d1c649b41146e8e9b5546
Souke <Souke.chaan@gmail.com>
2021-01-03 20:46:29 +0200

    Add files via upload

---
+++ .github/workflows/2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <iomanip>
+
+#include <time.h>
+
+
+using namespace std;
+
+int* randomItems(int size) {
+    int* arr = new int[size];
+
+    for (int i = 0; i < size; ++i) {
+        arr[i] = rand() % 10 - 10;
+        cout << setw(4) << arr[i];
+    }
+
+    cout << endl;
+    return arr;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand((unsigned)(time(NULL)));
+
+    int size, min = 3000, min_index;
+    cout << "IPZ 12 Tverdokhlib Angelina Bilet 11 Zav 2" << endl;
+    cout << "Введите размер массивов" << endl;
+    cin >> size;
+
+    int* arr1 = randomItems(size);
+    int* arr2 = randomItems(size);
+
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            if (arr1[i] == arr2[j] && min > arr1[i]) {
+                min = arr1[i];
+                min_index = i;
+            }
+        }
+    }
+
+    cout << "Наименьший элемент: " << min << " Индекс: " << min_index;
+
+    return 0;
+}
---
+++ .github/workflows/task 1.cpp
@@ -0,0 +1,53 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    cout << " IPZ 12 Tverdokhlib Angelina Bilet 11 Task 1" << endl;
+    cout << "Введите ряд символов" << endl;
+    getline(cin, str);
+
+    str.erase(
+        remove_if(str.begin(), str.end(), [](char c) {
+            string del = "AEIOUaeiou";
+            return isalpha(c) && del.find(c) != string::npos;
+            }), str.end()
+                );
+
+    cout << str << endl;
+
+    int* arr=new int[100];
+    int index = 0;
+    string tmp = "";
+
+
+    for (char c : str) {
+        if (isdigit(c)) {
+            tmp += c;
+        }
+        else if (tmp != "") {
+            arr[index] = stoi(tmp);
+
+            cout << " " << arr[index];
+
+            tmp = "";
+            index += 1;
+        }
+    }
+    if (tmp != "") {
+        arr[index] = stoi(tmp);
+
+        cout << " " << arr[index];
+    }
+    return 0;
+}
